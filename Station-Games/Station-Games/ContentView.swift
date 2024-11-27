eaefed880187428c7bc2e29dd2b2f4e718356092
Dias <dias.atudinov@gmail.com>
2024-11-27 16:08:52 +0500

    Create project

---
+++ Station-Games/Station-Games/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Station-Games
+//
+//  Created by Dias Atudinov on 27.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Station-Games/Station-Games/Station_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Station_GamesApp.swift
+//  Station-Games
+//
+//  Created by Dias Atudinov on 27.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Station_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
