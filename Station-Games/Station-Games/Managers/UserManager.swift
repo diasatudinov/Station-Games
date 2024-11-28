4eefeb28e649203546b1d27e81788422b30201a8
Dias <dias.atudinov@gmail.com>
2024-11-28 19:58:07 +0500

    User manager add

--- Station-Games/Station-Games/Managers/UserManager.swift
+++ Station-Games/Station-Games/Managers/UserManager.swift
@@ -5,4 +5,24 @@
 //  Created by Dias Atudinov on 28.11.2024.
 //
 
-import Foundation
+import SwiftUI
+
+class User: ObservableObject {
+    static let shared = User()
+    
+    @AppStorage("coins") var storedCoins: Int = 10
+    @Published var coins: Int = 10
+    
+    @AppStorage("game1Level") var storedLevel: Int = 1
+    @Published var level: Int = 1
+    
+    func updateUserCoins(for coins: Int) {
+        self.coins += coins
+        storedCoins = self.coins
+    }
+    
+    func updateUserLevel() {
+        self.level += 1
+        storedLevel = self.coins
+    }
+}
